1e5c469a1a49fe8c1cdfc02a230e2e8bbe1cdbdb
zake-dev <zake-dev@github.com>
2021-01-28 16:09:36 +0900

    ui: book details dialog

--- src/components/BookDetailsDialog/index.tsx
+++ src/components/BookDetailsDialog/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import { Dialog } from "@material-ui/core";
+import { Chip, Dialog, Typography } from "@material-ui/core";
+import BookmarkOutlinedIcon from "@material-ui/icons/BookmarkOutlined";
 
+import placeholder from "@assets/images/book-placeholder.jpg";
+import BookWithTags from "@interfaces/BookWithTags";
+import { getBookWithTagsEntity } from "@db/bookDataAccess";
 import { useStyles } from "./styles";
 
 type Props = {
@@ -11,6 +15,7 @@ type Props = {
 
 const BookDetailsDialog: React.FC<Props> = (props) => {
   const classes = useStyles();
+  const [book, setBook] = React.useState({} as BookWithTags);
   const { isbn, open, setOpen } = props;
 
   const handleClose = () => {
@@ -18,13 +23,50 @@ const BookDetailsDialog: React.FC<Props> = (props) => {
   };
 
   React.useEffect(() => {
-    const fetchData = async () => {};
+    const fetchData = async () => {
+      setBook(await getBookWithTagsEntity(isbn));
+    };
     fetchData();
   }, []);
 
   return (
     <Dialog open={open} onClose={handleClose}>
-      도서정보
+      <Typography className={classes.dialogTitle}>도서상세정보</Typography>
+      <div className={classes.row}>
+        <img src={placeholder} className={classes.placeholder} />
+        <div className={classes.infoCard}>
+          <Typography variant="h6" style={{ marginBottom: 5 }}>
+            {book.title}
+          </Typography>
+          <Typography>
+            <b>저자</b> {book.author}
+          </Typography>
+          <Typography>
+            <b>출판사</b> {book.press}
+          </Typography>
+          <Typography>
+            <b>ISBN</b> {book.isbn}
+          </Typography>
+          <Typography>
+            <b>정가</b>
+            {" \u20a9"}
+            {new Intl.NumberFormat("ko-KR").format(book.price)}
+          </Typography>
+          <Typography>
+            <b>재고</b> {book.quantity}권
+          </Typography>
+        </div>
+      </div>
+      <div className={classes.chipBox}>
+        <Chip
+          icon={<BookmarkOutlinedIcon />}
+          label={"1번 서가"}
+          color="secondary"
+        />
+        {book.tags.map((tag) => (
+          <Chip icon={<BookmarkOutlinedIcon />} label={tag} />
+        ))}
+      </div>
     </Dialog>
   );
 };
--- src/components/BookDetailsDialog/styles.tsx
+++ src/components/BookDetailsDialog/styles.tsx
@@ -2,7 +2,38 @@ import { makeStyles } from "@material-ui/core";
 
 export const useStyles = makeStyles((theme) => ({
   board: {
+    padding: theme.spacing(3),
+  },
+  row: {
     display: "flex",
-    margin: theme.spacing(3),
+    flexDirection: "row",
+  },
+  dialogTitle: {
+    padding: theme.spacing(1),
+    paddingLeft: theme.spacing(3),
+    paddingRight: theme.spacing(3),
+    backgroundColor: "#3f51b5",
+    color: "#ffffff",
+    fontSize: "1.3rem",
+    fontWeight: 700,
+  },
+  infoCard: {
+    display: "flex",
+    flexDirection: "column",
+    marginTop: theme.spacing(3),
+    marginRight: theme.spacing(3),
+  },
+  placeholder: {
+    width: "15rem",
+  },
+  chipBox: {
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    marginBottom: theme.spacing(2),
+
+    "& > *": {
+      margin: theme.spacing(0.5),
+    },
   },
 }));
