1a9087599e0fabb93756004b8a9fb87abed7be07
zake-dev <zake-dev@github.com>
2021-02-07 13:20:32 +0900

    feat: error alert from barcode input(sell page)

--- src/pages/Sell/InputBoard/index.tsx
+++ src/pages/Sell/InputBoard/index.tsx
@@ -6,6 +6,7 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import { useGlobalDispatch, useGlobalState } from "@reducers/GlobalStates";
 import { InventoryStateProvider } from "@reducers/InventoryStates";
 import { getBookEntity } from "@db/bookDataAccess";
+import Toast from "@components/Toast";
 
 import SearchDialog from "../SearchDialog";
 import { useStyles } from "./styles";
@@ -16,6 +17,8 @@ const InputBoard = () => {
   const dispatch = useGlobalDispatch();
   const [isbn, setIsbn] = React.useState("");
   const [searchOpen, setSearchOpen] = React.useState(false);
+  const [toastOpen, setToastOpen] = React.useState(false);
+  const [message, setMessage] = React.useState("");
 
   const handleClearList = () => {
     dispatch({ type: "REFRESH_SELL_WITH", list: [] });
@@ -23,13 +26,35 @@ const InputBoard = () => {
 
   const handleSubmit = async () => {
     // Input Error - 문자 입력
-    if (isNaN(parseInt(isbn))) return;
+    if (isNaN(parseInt(isbn))) {
+      setToastOpen(false);
+      setMessage("숫자만 입력해주세요.");
+      setToastOpen(true);
+      return;
+    }
     // Input Error - ISBN 길이 오류
-    if (isbn.length != 13) return;
+    if (isbn.length != 13) {
+      setToastOpen(false);
+      setMessage("유효하지 않은 ISBN입니다. (13자리 숫자)");
+      setToastOpen(true);
+      return;
+    }
 
     let book = await getBookEntity(isbn);
+    // DB Error - 등록되지 않은 도서
+    if (!book) {
+      setToastOpen(false);
+      setMessage("등록되지 않은 도서입니다. 먼저 입고해주세요.");
+      setToastOpen(true);
+      return;
+    }
     // DB Error - 수량부족
-    if (book.quantity == 0) return;
+    if (book.quantity == 0) {
+      setToastOpen(false);
+      setMessage("수량이 부족한 도서입니다. 먼저 입고해주세요.");
+      setToastOpen(true);
+      return;
+    }
 
     // 이미 리스트에 존재하는지 탐색
     let existingIndex = -1;
@@ -119,6 +144,12 @@ const InputBoard = () => {
           <SearchDialog open={searchOpen} setOpen={setSearchOpen} />
         </InventoryStateProvider>
       )}
+      <Toast
+        open={toastOpen}
+        setOpen={setToastOpen}
+        severity="error"
+        message={message}
+      />
     </>
   );
 };
