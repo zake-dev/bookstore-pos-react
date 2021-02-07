0baaa8963063f0d22b429e629801af8e27ad5c3b
zake-dev <zake-dev@github.com>
2021-02-07 12:27:24 +0900

    feat: sell button logic

--- src/pages/Sell/SellButton/index.tsx
+++ src/pages/Sell/SellButton/index.tsx
@@ -3,22 +3,51 @@ import clsx from "clsx";
 
 import { Button } from "@material-ui/core";
 import CreditCardIcon from "@material-ui/icons/CreditCard";
+import { useGlobalState, useGlobalDispatch } from "@reducers/GlobalStates";
+import Toast from "@components/Toast";
+import { updateBookEntity } from "@db/bookDataAccess";
 
 import { useStyles } from "./styles";
 
 const SellButton = () => {
   const classes = useStyles();
+  const dispatch = useGlobalDispatch();
+  const { sellList } = useGlobalState();
+  const [toastOpen, setToastOpen] = React.useState(false);
+  const [message, setMessage] = React.useState("");
+
+  const handleClick = () => {
+    if (!sellList) return;
+
+    sellList.forEach(async (book) => {
+      book.quantity -= book.currentQuantity;
+      await updateBookEntity(book);
+    });
+
+    setMessage(`총 ${sellList.length}권의 도서를 판매했습니다.`);
+    dispatch({ type: "REFRESH_SELL_WITH", list: [] });
+    setToastOpen(true);
+  };
 
   return (
-    <Button
-      className={clsx(classes.button, classes.sellButton)}
-      variant="contained"
-    >
-      <div className={classes.column}>
-        <CreditCardIcon className={classes.sellButtonIcon} />
-        판매하기
-      </div>
-    </Button>
+    <>
+      <Button
+        className={clsx(classes.button, classes.sellButton)}
+        variant="contained"
+        onClick={handleClick}
+      >
+        <div className={classes.column}>
+          <CreditCardIcon className={classes.sellButtonIcon} />
+          판매하기
+        </div>
+      </Button>
+      <Toast
+        open={toastOpen}
+        setOpen={setToastOpen}
+        severity="success"
+        message={message}
+      />
+    </>
   );
 };
 
